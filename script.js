528c8d0e9db6e4c876256b377a9ca4857fa22bf8
Aamir <156203722+AamirWar@users.noreply.github.com>
2026-04-08 12:21:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Select all drum buttons
+let buttons = document.querySelectorAll(".drum");
+
+// Add click event to each button
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        let key = this.innerHTML.toLowerCase();
+        makeSound(key);
+        buttonAnimation(key);
+    });
+}
+
+// Add keyboard event
+document.addEventListener("keydown", function (event) {
+    let key = event.key.toLowerCase();
+    makeSound(key);
+    buttonAnimation(key);
+});
+
+// Function to play sound
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            new Audio("Sounds/Snare.mp3").play();
+            break;
+
+        case "s":
+            new Audio("Sounds/kick.mp3").play();
+            break;
+
+        case "a":
+            new Audio("Sounds/HiHatClosed.mp3").play();
+            break;
+
+        case "d":
+            new Audio("Sounds/HiHatOpen.mp3").play();
+            break;
+
+        case "j":
+            new Audio("Sounds/CrashCymbal.mp3").play();
+            break;
+
+        case "k":
+            new Audio("Sounds/TomHit.mp3").play();
+            break;
+
+        default:
+            console.log("Unknown key:", key);
+    }
+}
